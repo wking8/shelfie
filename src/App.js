54f8daccd57d03048723ef6fb008117ec67aca66
Wyatt King <wyattking@Wyatts-MacBook-Pro.local>
2019-07-26 10:05:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
+import Header from './components/header/Header';
+import Form from './components/form/form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard />
+        <Form />
+        <Header />
+
+      </div>
+    );
+  }
 }
 
-export default App;
+
